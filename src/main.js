dc216208309d73331f29de9cd580b42927bf53f5
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-08-10 09:56:54 +0300

    Implemented The Landing Page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify';
-
+import '@mdi/font/css/materialdesignicons.css'
+import  './plugins/base'
+import  './plugins/section'
+import  './plugins/userCore'
+import  './plugins/adminCore'
 Vue.config.productionTip = false
 
 new Vue({
