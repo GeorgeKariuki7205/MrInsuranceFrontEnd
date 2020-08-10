c0f27d9009b81fb43a69c36870888b2c8980e125
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-08-10 15:20:54 +0300

    Implemented Navigation

--- src/components/userCore/AppBar.vue
+++ src/components/userCore/AppBar.vue
@@ -7,16 +7,19 @@
       color="white"
       elevation="1"
       height="80"
-    >     
+    >   
+    <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" />
       <h1 style="font-family:'cursive'">Mr Insurance</h1>
       <v-spacer />
 
       <div color="gray">
-        <v-tabs class="hidden-sm-and-down" optional>
+        <v-tabs class="hidden-sm-and-down icons-and-text" optional>
           <v-tab
+            
             v-for="(name, i) in items"
             :key="i"
             :to="{ name }"
+            
             :exact="name === 'Home'"
             :ripple="false"
             active-class="text--primary"
@@ -24,15 +27,18 @@
             min-width="96"
             text
           >
+            <v-icon>{{icons[i]}}</v-icon>
+            <v-spacer></v-spacer>
             {{ name }}
+            
           </v-tab>
         </v-tabs>
       </div>
 
-      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" />
+      
     </v-app-bar>
 
-    <home-drawer v-model="drawer" :items="items" />
+    <home-drawer v-model="drawer" :items="items" :icons="icons"/>
   </div>
 </template>
 
@@ -45,12 +51,18 @@ export default {
   },
 
   data: () => ({
-    drawer: null,
+    drawer: false,
+
     items: [
       "Home",
       "About",
-      "Contact",     
+      "Contact",
     ],
+    icons:[
+      "home",
+      "group",
+      "call"
+    ]         
   }),
 };
 </script>
--- src/components/userCore/Drawer.vue
+++ src/components/userCore/Drawer.vue
@@ -1,43 +1,60 @@
 <template>
   <v-navigation-drawer
-    bottom
-    color="transparent"
+    color="white"
     fixed
-    height="auto"
-    overlay-color="secondary"
-    overlay-opacity=".8"
     temporary
     v-bind="$attrs"
     v-on="$listeners"
-  >
-    <v-list
-      color="white"
-      shaped
-    >
+  >    
+    <v-list nav app>
+      <v-list-item-group active-class="primary--text text--accent-4">
+        <v-list-item two-line>
+      <v-list-item-avatar>
+        <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-list-item-title>Mr Insurance</v-list-item-title>
+        <v-list-item-subtitle>Insure Yourself Now.</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list color="white" shaped>
       <v-list-item
-        v-for="name in items"
-        :key="name"
+       v-for="(name, i) in items"
+        :key="i"
         :to="{ name }"
         :exact="name === 'Home'"
         color="primary"
       >
+      <v-list-item-icon>
+                <v-icon>{{icons[i]}}</v-icon>
+              </v-list-item-icon>
         <v-list-item-content>
           <v-list-item-title v-text="name" />
         </v-list-item-content>
       </v-list-item>
     </v-list>
+    <v-divider></v-divider>
+      </v-list-item-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-  export default {
-    name: 'HomeDrawer',
+export default {
+  name: "HomeDrawer",
 
-    props: {
-      items: {
-        type: Array,
-        default: () => ([]),
-      },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
     },
-  }
+    icons: {
+      type: Array,
+      default: () => [],
+    },
+    
+  },
+};
 </script>
