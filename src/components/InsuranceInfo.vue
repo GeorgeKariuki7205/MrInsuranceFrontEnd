f0a2f82519d2b8a81efcb0bfc8cd0064adce9448
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-08-12 12:08:46 +0300

    Implemented the footer data in the application

--- src/components/InsuranceInfo.vue
+++ src/components/InsuranceInfo.vue
@@ -1,28 +1,41 @@
 <template>
-  <base-info-card title="Insurance Covers.">
-    <a
-      v-for="feature in features"
-      :key="feature"
-      :text="feature"
-      class="mb-8 grey--text body-1 d-block text-none"
-      href="#"
-      v-html="`&rsaquo; ${feature}`"
-    />
+<div>
+  <base-info-card title="Insurance Covers.">    
   </base-info-card>
+      <v-list background-color="transparent">
+      <v-list-item-group background-color="transparent">        
+        <v-list color="transparent">          
+          <v-list-group v-for="item in navigationStateGetter"
+            :key="item" prepend-icon="local_hospital">
+            <template v-slot:activator>
+              <v-list-item-title>{{ item["cover"] }}</v-list-item-title>
+            </template>                
+                <v-list-item v-for="item in item['subCategories']" :key="item" link>
+                <v-list-item-title v-text="item"></v-list-item-title>                
+              </v-list-item>                      
+          </v-list-group>           
+        </v-list>
+        <v-divider></v-divider>
+      </v-list-item-group>
+    </v-list>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'SectionFeaturesAlt',
-
-    data: () => ({
-      features: [
-        'Health Insurance',
-        'Life Insurance',
-        'Motor Insurance',
-        'Family Health',
-        'Golf insurance',
-      ],
-    }),
-  }
+import { mapGetters } from "vuex";
+export default {
+  name: "SectionFeaturesAlt",
+  computed: {
+    ...mapGetters(["navigationStateGetter"]),
+  },
+  data: () => ({
+    features: [
+      "Health Insurance",
+      "Life Insurance",
+      "Motor Insurance",
+      "Family Health",
+      "Golf insurance",
+    ],
+  }),
+};
 </script>
